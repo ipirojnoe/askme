458bbba914462535fc948093f21150b672cc6e30
Ilya <deminiiii@gmail.com>
2020-12-16 12:27:42 +0300

    Add some questions stubs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,17 +29,17 @@ class UsersController < ApplicationController
         '71269686e0f757ddb4f73614f43ae445?s=100'
     )
 
-    # Болванка вопросов для пользователя
     @questions = [
-      Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016')),
+      Question.new(text: 'Как дела?', answer: 'Норм', created_at: Date.parse('1.03.2021')),
       Question.new(
-        text: 'В чем смысл жизни?', created_at: Date.parse('27.03.2016')
-      )
+        text: 'В чем смысл жизни?', answer: '...',created_at: Date.parse('27.03.2016')
+      ),
+      Question.new(text: 'Да?', answer: 'Да', created_at: Date.parse('27.03.2016')),
+      Question.new(text: 'Да?', answer: 'Да', created_at: Date.parse('27.03.2016')),
+      Question.new(text: 'Да?', answer: 'Да', created_at: Date.parse('27.03.2016'))
+      
     ]
 
-    # Болванка для нового вопроса
     @new_question = Question.new
-
-    # Обратите внимание, пока ни одна из болванок не достается из базы
   end
 end
