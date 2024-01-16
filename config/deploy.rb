7ecc17313fa3cd2dbcefa3b96ce43843782201de
Ilya <deminiiii@gmail.com>
2024-01-16 17:43:22 +0300

    Link master.key in capistrano config

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :repo_url, "git@github.com:ipirojnoe/askme.git"
 
 set :deploy_to, "/home/client/askme"
 
-append :linked_files, 'config/database.yml', '.env'
+append :linked_files, 'config/database.yml', '.env', 'config/master.key'
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads'
 
 set :branch, 'main'
